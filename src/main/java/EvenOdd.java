60b9e03e0237637827d0b78acc005ec504cf7649
shriyog <shriyog.vikram@gmail.com>
2023-01-25 10:38:31 +0530

    Added java demo application files

---
+++ src/main/java/EvenOdd.java
@@ -0,0 +1,16 @@
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(CheckIfInputIsAnEvenNumber(122));
+		//testing in main method
+		
+	}
+	public static boolean  CheckIfInputIsAnEvenNumber (int number) {
+		
+		return number  % 2 == 0;
+		
+	}
+
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+	@Test
+	public void testInputIsEven() {
+		assertTrue(EvenOdd.CheckIfInputIsAnEvenNumber(23));
+	}
+	
+
+	}
+
+
